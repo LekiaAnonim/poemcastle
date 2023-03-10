906c41b4aebf1d6baf14f0afee976d4f0d7c5124
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-03-10 23:14:50 +0100

    removed sqlite databas

--- PoemCastle/settings.py
+++ PoemCastle/settings.py
@@ -86,12 +86,12 @@ SESSION_SAVE_EVERY_REQUEST = True
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     "default": dj_database_url.config(default='postgresql://postgres:Mt0qQMQsFkGgXoG8GdP7@containers-us-west-164.railway.app:6467/railway', conn_max_age=1800),
